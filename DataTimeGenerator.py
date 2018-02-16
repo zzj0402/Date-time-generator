48d6ec1b575f6768ff787d3ed48c9430e3427547
Zijing Zhang <zzj0402@gmail.com>
2018-02-16 17:25:06 +0800

    Automatic clipping
    Automatically copy to the clipboard

--- DataTimeGenerator.py
+++ DataTimeGenerator.py
@@ -1,2 +1,10 @@
 import time
-print time.strftime("%Y-%m-%d %H:%M")
+from Tkinter import Tk
+r = Tk()
+r.withdraw()
+r.clipboard_clear()
+dataTime=time.strftime("%Y-%m-%d %H:%M")
+print dataTime
+r.clipboard_append(dataTime)
+r.update() # now it stays on the clipboard after the window is closed
+r.destroy()
